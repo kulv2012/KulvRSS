1b8c87f41be4cc2576dd70ed623d644ef1b45214
kulv2012 <hw_henry2008@126.com>
2013-09-07 12:47:19 +0800

    initial commit for my rss, further compoment will add later based on my preference.

---
+++ conf/config.php
@@ -0,0 +1,19 @@
+<?php
+
+$config = array();
+
+$db = array( 
+        'type'     => 'mysql',
+        'user'     => 'myrss',
+        'pass'     => 'develop',
+        'host'     => 'localhost',
+        'port'     => 8306,
+        'socket'   => FALSE,
+        'database' => 'Db_Myrss'
+
+        );
+
+$config['db'] = $db ;
+
+
+
